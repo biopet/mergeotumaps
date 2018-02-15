64592c7490eb6949609ac07d50e45dda3202ae1f
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/mergeotumaps/MergeOtuMapsTest.scala
+++ src/test/scala/nl/biopet/tools/mergeotumaps/MergeOtuMapsTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.mergeotumaps
 
 import java.io.File
@@ -13,8 +34,15 @@ class MergeOtuMapsTest extends ToolTest[Args] {
   def testMain(): Unit = {
     val temp = File.createTempFile("out", ".txt")
     temp.deleteOnExit()
-    MergeOtuMaps.main(Array("-I", resourcePath("/fakeOtu1"), "-I", resourcePath("/fakeOtu2"),
-      "-o", temp.getAbsolutePath(), "-p", "skip_"))
+    MergeOtuMaps.main(
+      Array("-I",
+            resourcePath("/fakeOtu1"),
+            "-I",
+            resourcePath("/fakeOtu2"),
+            "-o",
+            temp.getAbsolutePath(),
+            "-p",
+            "skip_"))
     val contents = fromFile(temp).mkString
     val expected = fromFile(resourcePath("/outOtu")).mkString
     contents shouldBe expected
